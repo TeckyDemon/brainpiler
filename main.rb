7ace9727942fb5f0445196e4cce000f130198ff1
Michał Wróblewski <20270277+DeBos99@users.noreply.github.com>
2019-02-19 18:32:08 +0000

    Add main.rb

---
+++ main.rb
@@ -0,0 +1,22 @@
+require 'optparse'
+
+INSTRUCTIONS={
+	'brainfuck'=>['>','<','+','-','.',',','[',']'],
+	'blub'=>['Blub. Blub?','Blub? Blub.','Blub. Blub.','Blub! Blub!','Blub! Blub.','Blub. Blub!','Blub! Blub?','Blub? Blub!'].map{|x|"#{x} "},
+	'ook!'=>['Ook. Ook?','Ook? Ook.','Ook. Ook.','Ook! Ook!','Ook! Ook.','Ook. Ook!','Ook! Ook?','Ook? Ook!'].map{|x|"#{x} "},
+	'*brainfuck'=>['>+','>-','<+','<-','<.','<,','<[',']'],
+	'binaryfuck'=>['010','011','000','001','100','101','110','111']
+}
+
+OptionParser.new do |parser|
+	options={}
+	parser.on('-i','--input INPUT','Set <input> file for program'){|x|options[:input]=x}
+	parser.on('-o','--output OUTPUT','Set <output> file for program'){|x|options[:output]=x}
+	parser.on('-l','--lang LANG','Set <lang> for program') do |x|
+		options[:lang]=x
+		if not options[:input] or not options[:output] or not options[:lang] then
+			raise OptionParser::MissingArgument
+		end
+		File.open(options[:output],'w+').write(File.open(options[:input],'r').read.chars.map{|x|INSTRUCTIONS[options[:lang]][INSTRUCTIONS['brainfuck'].index(x)||INSTRUCTIONS[options[:lang]].size]||''}.join.strip)
+	end
+end.parse!
